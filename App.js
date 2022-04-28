1886cb1706b99833df0111fe0276703ba9623477
ArjunKumarDev <arjunkumarks98@gmail.com>
2022-04-28 23:14:32 +0530

    bottom tab navigator done

--- App.js
+++ App.js
@@ -1,22 +1,39 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { BookDetail, Home } from "./screens";
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text>Book Store App!</Text>
-      <Home />
-      <BookDetail />
-    </View>
-  );
-}
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import Tabs from "./navigation/tabs";
+import { BookDetail } from "./screens";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+console.log(DefaultTheme, "checking");
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    border: "transparent",
   },
-});
+};
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Home"
+      >
+        {/* Tabs */}
+        <Stack.Screen name="Home" component={Tabs} />
+
+        {/* Screens */}
+        <Stack.Screen
+          name="BookDetail"
+          component={BookDetail}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
